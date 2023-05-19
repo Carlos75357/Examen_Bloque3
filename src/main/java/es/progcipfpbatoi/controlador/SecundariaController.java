76abe3039cf92ceb88d769b320e54c55008943a0
Roberto Hidalgo <rhidalgo@cipfpbatoi.es>
2023-05-19 15:34:06 +0200

    Vista secundaria

---
+++ src/main/java/es/progcipfpbatoi/controlador/SecundariaController.java
@@ -0,0 +1,19 @@
+package es.progcipfpbatoi.controlador;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SecundariaController implements Initializable {
+
+    public SecundariaController() {
+        // Por implementar
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
